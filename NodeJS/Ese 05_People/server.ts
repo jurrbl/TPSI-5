0f56b56f6419fd75fb029dcdc501750622469ec2
jurrbl <124259718+jurrbl@users.noreply.github.com>
2024-10-08 17:05:28 +0200

    fixing with new dispatcher

---
+++ NodeJS/Ese 05_People/server.ts
@@ -0,0 +1,35 @@
+import * as http from 'http';
+import dispatcher from './dispatcher'; 
+import headers from './static/headers.json'; 
+import fs from 'fs';
+
+const PORT = 1337;
+
+// Leggi i dati delle persone da people.json
+
+
+const peopleData = JSON.parse(fs.readFileSync('./people.json', 'utf-8'));
+
+// Crea il server
+const server = http.createServer(function (req, res) {
+  // Aggiungi intestazioni CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Dispatcchia la richiesta
+  dispatcher.dispatch(req, res);
+});
+
+// Ascolta sulla porta specificata
+server.listen(PORT, function () {
+  console.log('Server in ascolto sulla porta: ' + PORT);
+});
+
+/***  Registrazione dei listener ***/
+dispatcher.addListener('GET', '/country?', function (req, res) {
+  res.writeHead(200, headers.json);
+  res.write(JSON.stringify(peopleData)); // Assicurati di inviare i dati delle persone
+    
+  res.end();
+});
---
+++ NodeJS/Ese 05_People/static/index.js
@@ -0,0 +1,24 @@
+"use strict"
+
+let currentCountry // nazione attualmente selezionata
+
+// vettore enumerativo delle Persone attualmente visualizzate
+// Comodo per gestire i pulsanti di navigazione
+let people;  
+// Posizione del dettaglio corrente (rispetto al vettore enumerativo people)     
+let currentPos;   
+
+
+$(document).ready(function() {
+    let _lstNazioni = $("#lstNazioni");
+    let _tabStudenti = $("#tabStudenti");
+    let _divDettagli = $("#divDettagli");
+
+    _divDettagli.hide();
+
+    // Effettua la richiesta per ottenere le nazioni
+    let rq = inviaRichiesta("GET", "/country")
+    rq.then(function(response){
+        console.log('ciao')
+    })
+});
