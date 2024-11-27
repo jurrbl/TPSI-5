09b6659aa01de56435a5ccb740de966b90da918a
Belba Jurgen <124259718+jurrbl@users.noreply.github.com>
2024-11-27 08:48:34 +0100

    Recipe Detail To DO

--- Angular/My-Recipes/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Angular/My-Recipes/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RecipeModel } from '../../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-item.component.css'
 })
 export class RecipeItemComponent {
-
+  @Input() recipe!: RecipeModel;
 }
