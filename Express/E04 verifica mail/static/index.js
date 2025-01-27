ab69ee8e92e3d6d596c1818babc6c1462a45540f
jurrbl <124259718+jurrbl@users.noreply.github.com>
2025-01-27 23:29:08 +0100

    Ciao

--- Express/E04 verifica mail/static/index.js
+++ Express/E04 verifica mail/static/index.js
@@ -1,27 +1,134 @@
-"use strict"
-$(document).ready(function() {
-	
-	let _login = $("#login")
-	let _mail = $("#mail")
+"use strict";
+$(document).ready(function () {
+    let _login = $("#login");
+    let _mail = $("#mail");
 
-	let _username = $("#usr")
-	let _password = $("#pwd")
-	let _lblErrore = $("#lblErrore")
-	let _btnInvia = $("#btnInvia");
+    let _username = $("#usr");
+    let _password = $("#pwd");
+    let _lblErrore = $("#lblErrore");
+    let _btnInvia = $("#btnInvia");
 
-	_mail.hide()
+    let txtTo = $("#txtTo");
+    let txtSubject = $("#txtSubject");
+    let txtMessage = $("#txtMessage");
+    let txtAttachment = $("#txtAttachment");
 
-    _lblErrore.hide();	
-	_lblErrore.children("button").on("click", function(){
-		_lblErrore.hide();
-	})
+    let Username = "";
+    let Password = "";
 
+    _mail.hide();
+    _lblErrore.hide();
 
+    _lblErrore.children("button").on("click", function () {
+        _lblErrore.hide();
+    });
 
+    // Login logic
+    $("#btnLogin").on("click", async function () {
+        Username = _username.val();
+        Password = _password.val();
 
+        if (Username === "" || Password === "") {
+            alert("Inserire Username e Password");
+            return;
+        }
 
+        const request = await inviaRichiesta("POST", "/api/login", {
+            username: Username,
+            password: Password,
+        });
 
+        if (request.status === 200) {
+            console.log(request.data);
+            _login.hide();
+            _mail.show();
 
+            // Popola la tabella
+            getTableBody();
+        } else {
+            alert("Login fallito, controllare username e password.");
+        }
+    });
 
+    // Send mail logic
+    $("#btnInvia").on("click", async function () {
+        let To = txtTo.val();
+        let Subject = txtSubject.val();
+        let Message = txtMessage.val();
+        let Attachment = txtAttachment.prop("files")[0]; // File selezionato
+
+        if (To === "" || Subject === "" || Message === "") {
+            alert("Inserire i campi obbligatori");
+            return;
+        }
+
+        try {
+            let formData = new FormData();
+            formData.append("from", Username); // Mittente
+            formData.append("to", To);
+            formData.append("subject", Subject);
+            formData.append("body", Message);
+            if (Attachment) {
+                formData.append("attachment", Attachment); // Aggiunge l'allegato solo se presente
+            }
+
+            const request2 = await inviaRichiesta("POST", "/api/sendMail", formData, true);
+
+            if (request2.status === 200) {
+                alert("Mail inviata correttamente");
+
+                // Aggiorna la tabella
+                getTableBody();
+
+                // Pulisce i campi di input
+                txtTo.val("");
+                txtSubject.val("");
+                txtMessage.val("");
+                txtAttachment.val("");
+            } else if (request2.status === 503) {
+                alert("Destinatario non valido");
+            } else {
+                alert("Errore durante l'invio della mail.");
+            }
+        } catch (err) {
+            console.error("Errore:", err);
+            alert("Errore imprevisto durante l'invio della mail.");
+        }
+    });
+
+    // Funzione per ottenere e aggiornare la tabella
+    async function getTableBody() {
+        const request = await inviaRichiesta("POST", "/api/login", {
+            username: Username,
+            password: Password,
+        });
+
+        if (request.status === 200) {
+			console.log(request.data);
+            let mailArray = request.data.mail.reverse();
+            let tbody = $("#tbody");
+            tbody.empty();
+
+            mailArray.forEach((element) => {
+                let tr = $("<tr>");
+                let td1 = $("<td>").text(element.from); // Mittente
+                let td2 = $("<td>").text(element.subject); // Oggetto
+                let td3 = $("<td>").text(element.body); // Contenuto
+                let td4;
+
+                if (element.attachment && element.attachment !== "") {
+                    td4 = $("<td>").html(
+                        `<a href="img/${element.attachment}" target="_blank">${element.attachment}</a>`
+                    );
+                } else {
+                    td4 = $("<td>").text("Nessun allegato");
+                }
+
+                tr.append(td1, td2, td3, td4);
+                tbody.append(tr);
+            });
+        } else {
+            alert("Errore durante il recupero delle mail.");
+        }
+    }
 });
-
--- Express/E05 google_instant/static/index.js
+++ Express/E05 google_instant/static/index.js
@@ -1,24 +1,139 @@
-'use strict'
+'use strict';
+let Username;
+let Password;
+
+let UsernameRegister;
+let PasswordRegister;
+let ImmagineProfiloRegister;
 
 $(document).ready(function () {
+  /* ***************** SEZIONE 1 ************************ */
+  let _btnAccedi = $("#btnAccedi");
+  let _btnRegistrati = $("#btnRegistrati");
+  let _divAccedi = $("#divAccedi").hide();
+  let _divRegistrati = $("#divRegistrati").hide();
+  let _imgProfilo = $("#imgProfilo").hide();
 
-	/* ***************** SEZIONE 1 ************************ */	
-	let _btnAccedi = $("#btnAccedi")
-	let _btnRegistrati = $("#btnRegistrati")
-	let _divAccedi = $("#divAccedi").hide()
-	let _divRegistrati = $("#divRegistrati").hide()
-	let _imgProfilo = $("#imgProfilo").hide()
-	
+  function handleCancel() {
+    _divAccedi.hide();
+    _divRegistrati.hide();
+  }
 
+  _btnAccedi.on("click", async function () {
+    _divRegistrati.hide();
+    _divAccedi.toggle();
 
+    $("#btnAnnulla").on("click", handleCancel);
+  });
 
+  $("#btnOk").on("click", async function () {
+    Username = $("#username").val();
+    Password = $("#password").val();
 
-	/* ***************** SEZIONE 2 ************************ */	
-	let _txtRicerca = $("#txtRicerca")	
-	let _lstSuggerimenti = $("#lstSuggerimenti").hide()
-	let _divLink = $("#divLink").hide()
+    console.log(Username, Password);
 
+    const request = await inviaRichiesta("POST", "/api/login", {
+      username: Username,
+      password: Password,
+    });
+    if (request.status == 200) {
+      _btnAccedi.hide();
+      _imgProfilo.show();
+      _imgProfilo.prop("src", `img/${Username}.jpg`);
+      _divAccedi.hide();
+    }
+  });
 
+  _btnRegistrati.on("click", function () {
+    _divAccedi.hide();
+    _divRegistrati.toggle();
 
-	
-})
+    $("#btnOkRegister").on("click", async function () {
+      UsernameRegister = $("#usernameRegister").val();
+      PasswordRegister = $("#passwordRegister").val();
+      ImmagineProfiloRegister = $("#imgProfiloFile").prop("files")[0];
+
+      if (!UsernameRegister || !PasswordRegister || !ImmagineProfiloRegister) {
+        alert("Compila tutti i campi!");
+        return;
+      }
+
+      let formData = new FormData();
+      formData.append("username", UsernameRegister);
+      formData.append("password", PasswordRegister);
+      formData.append("img", ImmagineProfiloRegister);
+
+      const request = await inviaRichiesta("POST", "/api/register", formData);
+
+      if (request.status == 200) {
+        alert("Registrazione avvenuta con successo");
+        _divRegistrati.hide();
+      } else if (request.status == 422) {
+        alert("Username già esistente");
+      }
+    });
+
+    $("#btnAnnulla").on("click", handleCancel);
+  });
+
+  $("#txtRicerca").on("keyup", async function () {
+    let txtRicerca = $("#txtRicerca").val().trim();
+    if (txtRicerca) {
+      try {
+        const url = `/api/ricerca?ricerca=${txtRicerca.split(' ').join('+')}`;
+        const request = await inviaRichiesta("GET", url);
+
+        if (request.status === 200) {
+          let lstSuggerimenti = $("#lstSuggerimenti");
+          lstSuggerimenti.empty().show();
+
+          // Aggiungi opzioni dinamiche con evento click
+          for (let voce of request.data.voci) {
+            $("<option>")
+              .text(voce.voci.join(", "))
+              .val(voce._id)
+              .appendTo(lstSuggerimenti)
+              .on("click", async function () {
+                // Copia la voce selezionata nel campo di ricerca
+                $("#txtRicerca").val($(this).text());
+
+                // Nasconde il ListBox
+                lstSuggerimenti.hide();
+
+                // Mostra il contenitore divLink
+                $("#divLink").show();
+
+                // Invio della richiesta PATCH al server per incrementare nClick
+                try {
+                  const patchRequest = await inviaRichiesta("PATCH", "/api/click", {
+                    id: $(this).val(),
+                  });
+
+                  if (patchRequest.status === 200) {
+                    // Visualizza i dati ricevuti nel divLink
+                    let divLink = $("#divLink");
+                    divLink.empty(); // Svuota il contenitore
+                    for (let link of patchRequest.data.links) {
+                      $("<p>").text(link.description).appendTo(divLink);
+                      $("<a>")
+                        .attr("href", link.url)
+                        .attr("target", "_blank")
+                        .text(link.url)
+                        .appendTo(divLink);
+                    }
+                  }
+                } catch (err) {
+                  console.error("Errore durante la richiesta PATCH:", err);
+                }
+              });
+          }
+        }
+      } catch (err) {
+        console.error("Errore durante la ricerca:", err);
+      }
+    } else {
+      // Nascondi suggerimenti se il campo è vuoto
+      $("#lstSuggerimenti").empty().hide();
+    }
+  });
+});
